3e0d646c415a956e580406f296f18b3db9d56c02
Dekel Livyani <DekelLiv0@gmail.com>
2021-06-23 15:11:43 +0300

    add debounce

--- js/services/util-service.js
+++ js/services/util-service.js
@@ -1,61 +1,37 @@
 export const utilService = {
-<<<<<<< HEAD
-  load: loadFromStorage,
-  save: saveToStorage,
-  getRandomInt,
-  debounce,
-  makeId,
-};
-=======
     load: loadFromStorage,
     save: saveToStorage,
     getRandomInt,
+    debounce,
     makeId,
-}
->>>>>>> 4ef79747c6704a1e3a3635b442efd4979372ce27
+};
 
 function loadFromStorage(key) {
-  var val = localStorage.getItem(key);
-  return JSON.parse(val);
+    var val = localStorage.getItem(key);
+    return JSON.parse(val);
 }
 
 function saveToStorage(key, val) {
-  localStorage.setItem(key, JSON.stringify(val));
+    localStorage.setItem(key, JSON.stringify(val));
 }
 
 function getRandomInt(min, max) {
-<<<<<<< HEAD
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function debounce(func, wait = 1000) {
-  let timeout;
-
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-}
-
-function makeId(length = 7) {
-  var txt = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < length; i++) {
-    txt += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return txt;
-}
-=======
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function debounce(func, wait = 1000) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
 }
 
 function makeId(length = 7) {
@@ -66,4 +42,3 @@ function makeId(length = 7) {
     }
     return txt;
 }
->>>>>>> 4ef79747c6704a1e3a3635b442efd4979372ce27
