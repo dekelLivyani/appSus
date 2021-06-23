348ef617a494c4d0f3e768c8452f56bdf7b80327
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-23 10:54:13 +0300

    added pages header footer note-app

---
+++ js/apps/note/pages/note-app.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+    <section class="note-app">
+        <h1 class="note-app-title">Welcome to Note!</h1>
+    </section>
+    `,
+};
---
+++ js/cmps/app-footer.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+        <section class="app-footer">
+            <p class="footer-txt">© Dekel Livyani & Ilya Grigoriev Starr</p>
+        </section>
+        `,
+};
