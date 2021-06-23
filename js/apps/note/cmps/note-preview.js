269281c65897585953bc4daa2c3c7507907e7ea8
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-23 17:10:27 +0300

    sync commit

--- js/apps/note/cmps/note-preview.js
+++ js/apps/note/cmps/note-preview.js
@@ -1,3 +1,6 @@
+import { noteService } from '../services/note-service.js';
+
+noteService;
 export default {
   props: ['note'],
   template: `
@@ -5,9 +8,13 @@ export default {
             <router-link :to="noteURL" class="note-preview">
                 <h3 class="note-title">{{note.info.title}}</h3>
                 <p class="note-txt">{{note.info.txt}}</p>
+                <button class="removeNote" @click="removeNote">✖</button>
             </router-link>
         </li>       
     `,
+  methods: {
+    removeNote() {},
+  },
   computed: {
     noteURL() {
       return `/note/${this.note.id}`;
