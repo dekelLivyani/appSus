bf00f6ac3724ae716770486e5e1f64d79d255c7c
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-26 16:10:54 +0300

    fixed color modal position bug

--- js/apps/note/services/notes-init-service.js
+++ js/apps/note/services/notes-init-service.js
@@ -16,6 +16,17 @@ const notesInitNew = [
     created: 1624612963984,
     lastEdited: 1624612963984,
     type: 'noteImg',
+    color: 'lightgreen',
+    info: {
+      title: 'Agent P',
+      url: 'https://static01.nyt.com/images/2021/01/19/science/09TB-PLATYPUS/09TB-PLATYPUS-superJumbo.jpg',
+    },
+  },
+  {
+    id: 'afdadsfd',
+    created: 1624612963984,
+    lastEdited: 1624612963984,
+    type: 'noteImg',
     color: 'whitesmoke',
     info: {
       title: 'Agent P',
