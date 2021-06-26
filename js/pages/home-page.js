daaf7e20c061db16084ae2c4bb825a2ba34ca94e
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-26 13:29:07 +0300

    improved homepage

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -2,10 +2,12 @@ export default {
   template: `
    <section class="home-page">
    <img src="./img/home-page-hero.jpg" />
-   <h1 class="title">The future of communication is here:</h1>
-   <div class="homepage-nav">
-      <div class="link"> <router-link class="link email-link" to="/email"></router-link> </div>
-      <div class="link"> <router-link class="link note-link" to="/noteDyn"></router-link></div>
+   <div class="homepage-content">
+       <h1 class="title">The future of communication is here:</h1>
+       <div class="homepage-nav">
+          <div class="link" title="Email"> <router-link class="link email-link" to="/email"></router-link> </div>
+          <div class="link" title="Note"> <router-link class="link note-link" to="/noteDyn"></router-link></div>
+       </div>
    </div>
 </section>
 `,
