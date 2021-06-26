3bd9bd5c6dda137c16f4d25263621d1dd3d904b6
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-26 13:06:37 +0300

    editing homepage

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -1,8 +1,12 @@
 export default {
-    template: `
+  template: `
    <section class="home-page">
    <img src="./img/home-page-hero.jpg" />
-   <h1 class="title">Ilya I let you choose a good title😉 </h1>
+   <h1 class="title">The future of communication is here:</h1>
+   <div class="homepage-nav">
+      <div class="link"> <router-link class="link email-link" to="/email"></router-link> </div>
+      <div class="link"> <router-link class="link note-link" to="/noteDyn"></router-link></div>
+   </div>
 </section>
 `,
-}
+};
